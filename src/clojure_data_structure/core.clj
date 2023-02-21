ccc71eb2ac2b2ab391d719a0263924da4fccdf31
Helder <helder.deoliveira@codurance.com>
2023-02-21 16:21:18 +0100

    fizzbuzz

--- src/clojure_data_structure/core.clj
+++ src/clojure_data_structure/core.clj
@@ -14,5 +14,22 @@
 (def y [1 2 3 4])
 (def z #{1 2 3 4})
 (def p {:name "Helder" :surname "De oliveira"})
+
 (map inc y)
+
 (s/union #{2 3} #{1 2})
+
+(defn innerfizzbuzz [n]
+  (let [divisible-by?
+        (fn [div]
+          (= 0 (mod n div)))]
+    (cond
+      (divisible-by? 15) "FizzBuzz"
+      (divisible-by? 3)  "Fizz"
+      (divisible-by? 5)  "Buzz"
+      :else              n)))
+
+(defn fizzbuzz [ns]
+  (map innerfizzbuzz ns))
+
+(fizzbuzz '(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20))
